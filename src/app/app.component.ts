202b68c3377c6922bef5175f33f014bf5c022128
CFH00893248 <cfh00893248@CFH00893248de-MacBook-Pro.local>
2023-08-14 17:23:55 +0800

    add table dropdown list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,6 +46,16 @@ export class AppComponent {
   columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
   expandedElement!: PeriodicElement | null;
   logout(): void {}
+  dataSource1 = ELEMENT_DATA1;
+  columnsDetail = [
+    '項目',
+    '整年預計使用',
+    '目前應發生',
+    '目前已發生',
+    '年度使用率',
+    '累積使用率',
+    '狀態',
+  ];
 }
 
 export interface PeriodicElement {
@@ -66,12 +76,39 @@ const ELEMENT_DATA: PeriodicElement[] = [
     目前應發生: 15350000,
     目前已發生: 2247000,
     年度使用率: '7.23%',
-    累積使用率: '14.64',
+    累積使用率: '14.64%',
     狀態: '注意',
     description: 'test',
   },
   {
     科目: '其他費用',
+    整年預計使用: 67080000,
+    目前應發生: 43276664,
+    目前已發生: 17055860,
+    年度使用率: '25.43%',
+    累積使用率: '39.41%',
+    狀態: '正常',
+    description: `test`,
+  },
+];
+export interface PeriodicElement1 extends PeriodicElement{
+  項目: string;
+}
+const ELEMENT_DATA1: PeriodicElement1[] = [
+  {
+    科目: '資訊',
+    項目: '專業分析報告及產業資料庫',
+    整年預計使用: 31100000,
+    目前應發生: 15350000,
+    目前已發生: 2247000,
+    年度使用率: '7.23%',
+    累積使用率: '14.64%',
+    狀態: '注意',
+    description: 'test',
+  },
+  {
+    科目: '其他費用',
+    項目: '委外開發人力',
     整年預計使用: 67080000,
     目前應發生: 43276664,
     目前已發生: 17055860,
